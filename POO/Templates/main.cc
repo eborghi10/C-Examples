33f8398c10f3fdc6e55d70231773b5317505196c
eborghi10 <emiliano_borghi@hotmail.com>
2016-12-28 08:48:09 -0300

    Add template example

--- POO/Templates/main.cc
+++ POO/Templates/main.cc
@@ -25,6 +25,23 @@ using namespace std;
  ***** DECLARACIONES DE CLASES *****
  ***********************************/
 
+template <class T>
+class Bucky
+{
+	T first, second;	// Private variables
+public:
+	Bucky(T a, T b){	// Constructor
+		first = a;		// Assignment to priv. vars
+		second = b;
+	};	
+	
+	T bigger();	
+};
+
+template <class T>
+T Bucky<T>::bigger() {
+	return first > second? first : second;
+}
 
 /************************
  ***** FUNCIÓN MAIN *****
@@ -33,6 +50,18 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	// http://c.conclase.net/curso/index.php?cap=040#inicio
+	// https://www.youtube.com/watch?v=U2QvTsMvWmM
+
+	int x = 69, y = 105;
+	Bucky <int> bo(x, y);	// Calling the constructor...
+
+	/*
+	 * NOTE: bigger() doen't take arguments because it uses private
+	 * variables of the class "Bucky".
+	 */
+	cout << "The biggest between " << x << " and " << y <<
+	 " is: " << bo.bigger() << endl;
+
 	return 0;
 }
 
